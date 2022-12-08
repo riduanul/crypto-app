bcab2925a41a7f2e306a1c68e425f25ab62e8531
riduanul <riduanul.haque1@gmail.com>
2022-12-08 13:44:49 +0600

    some ui style changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import avatarimg from "../assets/pp.jpg";
 const Footer = () => {
   return (
-    <div className=" border-t-2 border-neutral-300 mt-10  px-10 py-10 bg-slate-900 text-white w-full text-orange-500">
+    <div className=" border-t-2 border-neutral-300 px-10 py-10 bg-slate-900 text-white w-full text-orange-500">
       <div className="  h-full flex flex-col md:flex-row  justify-between items-center">
         <div className="text-">
           <p className="font-bold">About Us</p>
-          <p className="font-small">
+          <small className="font-small">
             We are the best crypto trading app in Bangladesh, We provide our
             guidance at a very cheap price.
-          </p>
-          <p>Contact: +8801700000000</p>
+          </small>
+          <small>Contact: +8801700000000</small>
         </div>
         <div className="text-center ">
           <img
